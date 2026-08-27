ffaafa05ea477f48f5dc2bc1bb6d933094641c13
Pedro Veloso <86307207+pedrohaveloso@users.noreply.github.com>
2026-08-27 15:23:44 -0300

    feat: update dependencies and add layout structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Poppins",
+      cssVariable: "--font-poppins",
+      weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+    },
+  ],
+});
